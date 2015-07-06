0a15eee00cd984ac7dbf5bd81602b449d310da51
nikhil12punnam <nikhil12punnam@gmail.com>
2015-07-06 00:15:07 -0700

    Script change

--- basic_code.sh
+++ basic_code.sh
@@ -1,4 +1,4 @@
- #!/bin/bash
+#!/bin/bash
 #initializing variables
 bit=32
 bitv="x86_64"
@@ -6,15 +6,11 @@ source=".rpm"
 model="Ubuntu"
 extra="jdk"
 
-#setting proxy details
-export http_proxy="http://proxy.iiit.ac.in:8080"
-export https_proxy="http://proxy.iiit.ac.in:8080"
-
 #checking for system version
 uname -m > vers.txt
 while read -r li; do
 if [ $bitv = $li ]
-then
+then 
 bit=64
 break;
 else
@@ -26,17 +22,13 @@ done < vers.txt
 cat /etc/issue > vers.txt
 while read -r li; do
 if echo "$li" | grep -q "$model";then
-echo "It is Ubuntu Machine"
-#checking for updates and wget installation
+echo "Updating this Ubuntu Machine";
 sudo apt-get update
-sudo apt-get install wget
 break;
-else
-echo "It is CentOs Machine"
-#checking for updates and wget installation
-sudo yum update
-sudo yum install wget
+else 
+echo "Updating this CentOs Machine";
 model="Centos"
+sudo yum update
 break;
 fi
 done < vers.txt
@@ -47,19 +39,19 @@ while read -r line; do
 lineno=`expr $lineno + 1`
 done < basic.txt
 
-#reading given text file with nth line /////////////////////////////////WHILE LOOP BEGINS//////////////////////////////////////////////////////
-i=0
+#reading given text file with nth line
+i=0 
 while [ $i -lt $lineno ]; do
 i=`expr $i + 1`
 head -$i basic.txt | tail -1 > vers.txt
 read -r line < vers.txt
 line="$line-linux"
 if test $bit -eq 32
-then
+then 
 line="$line-x86"
 else
 line="$line-x64"
-fi
+fi 
 
 if [ $model = "Ubuntu" ]
 then
@@ -74,7 +66,8 @@ fi
 #Installing
 if [ $model = "Ubuntu" ]
 then
-#checking for proxy settings
+#checking for wget installation and proxy settings
+sudo apt-get install wget
 export http_proxy=""
 wget -r --no-parent 10.4.15.172/$line/
 cd 10.4.15.172/$line/
@@ -82,40 +75,43 @@ dpkg -i *.deb
 success=`echo $?`
 cd -
 else
-#checking for proxy settings
+#checking for wget installation and proxy settings
+sudo yum install wget
 export http_proxy=""
 wget -r --no-parent 10.4.15.172/$line/
 cd 10.4.15.172/$line/
-rpm -ivh *.rpm
+echo "HERE !---------------------------------------------------------------------------"
+echo pwd
+yum install *.rpm
+echo "HERE @---------------------------------------------------------------------------"
 success=`echo $?`
 cd -
-fi
+echo pwd
+fi 
 #DISPLAYING SUCCESS MSG
 #echo "\nSuccess = $success"
 
-if test $success -eq 0
+if test $success -eq 0 
 then
 #now to set path...
 if echo "$line" | grep -q "$extra";then
 echo "JAVA_HOME=/usr/lib/jvm/$lback" >> "/etc/profile"
 echo "PATH=$PATH:$HOME/bin:$JAVA_HOME/bin" >> "/etc/profile"
-echo "export JAVA_HOME" >> "/etc/profile"
+echo "export JAVA_HOME" >> "/etc/profile" 
 echo "export JRE_HOME" >> "/etc/profile"
-echo "export PATH" >> "/etc/profile"
+echo "export PATH" >> "/etc/profile" 
 fi
-else
+else 
 echo "------------------------------------------INSTALLING DEPENDENCIES---------------------------------"
 
 if [ $model = "Ubuntu" ]
 then
 export http_proxy="http://proxy.iiit.ac.in:8080"
 sudo apt-get -f install
-export http_proxy=""
 else
 export http_proxy="http://proxy.iiit.ac.in:8080"
-#sudo yum localinstall $line
 sudo yum update
-export http_proxy=""
+#sudo yum localinstall $line
 fi
 
 echo "------------------------------------------INSTALLED DEPENDENCIES-----------------------------------"
@@ -124,14 +120,13 @@ fi
 
 done
 
-#////////////////////////////////////////////////////WHILE LOOP ENDS///////////////////////////////////////////////
-
 #having old proxy
 export http_proxy="http://proxy.iiit.ac.in:8080"
-
-
 
 pwd > vers.txt
 read -r li < vers.txt
 rm $li/vers.txt
-#rm -R $line                                                                                                                   1,1           Top
+#rm -R $line
+
+
+
